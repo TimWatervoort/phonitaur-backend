d264bfdccde4604f50a1336fe3a97b0f02dbc2a1
Tim Watervoort <twwatervoort@gmail.com>
2019-01-10 16:47:05 -0700

    now sending json

--- questions/views.py
+++ questions/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+from django.http import JsonResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('The future home of the questions endpoint!')
+    return JsonResponse({'message': 'The future home of the questions endpoint!'})
