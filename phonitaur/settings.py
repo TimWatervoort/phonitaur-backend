45c0da55bcca3458b02e9df16883626394e0a6d0
Tim Watervoort <twwatervoort@gmail.com>
2019-01-17 13:33:19 -0700

    added simplejwt for auth

--- phonitaur/settings.py
+++ phonitaur/settings.py
@@ -127,5 +127,8 @@ STATIC_URL = '/static/'
 REST_FRAMEWORK = {
     'DEFAULT_PARSER_CLASSES': (
         'rest_framework.parsers.JSONParser',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 }
--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
+from django.urls import include, path
 
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 
 urlpatterns = [
@@ -9,5 +13,8 @@ urlpatterns = [
     path('alphabets/', views.languages, name='languages'),
     path('alphabets/<int:pk>', views.oneAlphabet, name='one alphabet'),
     path('lessons/<int:lang_id>', views.lessons, name='lessons'),
-    path('lesson/<int:lesson_id>', views.lesson, name='lesson')
+    path('lesson/<int:lesson_id>', views.lesson, name='lesson'),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
 ]
